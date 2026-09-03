5ea90ffd0d0905e753dedea390d9f3d85338a2a8
lam.duonghoangvu <lamthedev@proton.me>
2026-09-03 14:37:49 +0800

    Move theme css to config and add automated boostrap symlink

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,8 +2,3 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 "$SCRIPT_DIR/config/git.sh"
-"$SCRIPT_DIR/config/tmux.sh"
-"$SCRIPT_DIR/config/zen.sh"
-"$SCRIPT_DIR/config/beeper.sh"
-"$SCRIPT_DIR/config/better_discord.sh"
-"$SCRIPT_DIR/config/macos.sh"
