ea15b9f06d1524faf8611446f069d81f24a19354
lam.duonghoangvu <lamthedev@proton.me>
2026-09-25 20:28:02 +0800

    Simplify zen config again

--- config/zen/user.js
+++ config/zen/user.js
@@ -1,8 +1,5 @@
 // Enable User Customizationser
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("svg.context-properties.content.enabled", true);
-user_pref("layout.css.color-mix.enabled", true);
-user_pref("layout.css.backdrop-filter.enabled", true);
 
 // Browser
 user_pref("browser.warnOnQuitShortcut", false);
@@ -13,8 +10,6 @@ user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
 user_pref("zen.view.show-clear-tabs-button", false);
 
 // New Tab
-user_pref("zen.urlbar.replace-newtab", false);
-user_pref("zen.view.show-newtab-button-top", false);
 user_pref("zen.tabs.show-newtab-vertical", false);
 user_pref("browser.tabs.insertRelatedAfterCurrent", true);
 
@@ -31,6 +26,3 @@ user_pref("toolkit.telemetry.enabled", false);
 user_pref("toolkit.telemetry.archive.enabled", false);
 user_pref("browser.discovery.enabled", false);
 user_pref("browser.ping-centre.telemetry", false);
-
-// Zen Specific UI & Accent Preferences
-user_pref("zen.workspaces.show-workspace-indicator", true);
